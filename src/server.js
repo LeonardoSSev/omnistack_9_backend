f51895c1dd1b87eeaa092eb7baa5b493a95e8a56
Leonardo Santos Severino <leonardossev@gmail.com>
2019-10-27 03:44:54 -0300

    Uses cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express') ;
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const { stringConnection } = require('../databaseConfig');
 
@@ -11,6 +12,7 @@ mongoose.connect(stringConnection,{
     useUnifiedTopology: true
 })
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
